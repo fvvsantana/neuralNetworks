5277753c4b3486f1374106bb7438ff88fbad6968
Flavio Santana <fvvsantana@gmail.com>
2020-03-18 16:45:23 -0300

    Implement data visualization of the dataset

---
+++ ex3_perceptronAndAdaline/ex3/inc/adaline.py
@@ -0,0 +1,20 @@
+import numpy as np
+from inc.neuron import Neuron
+import inc.activationFunctions as af
+
+class Adaline(Neuron):
+    def __init__(self, weights = None, bias = None):
+        super().__init__(weights, bias, af.sigmoid)
+
+    def run(self, inputs):
+        # Put bias to arrays
+        weights = list(self.weights)
+        weights.insert(0, 1)
+        inputs = list(inputs)
+        inputs.insert(0, self.bias)
+
+        # Scalar product of arrays
+        scalarProduct = np.dot(weights, inputs)
+
+        # Apply activation function
+        return self.af(scalarProduct)
---
+++ ex3_perceptronAndAdaline/ex3/main.py
@@ -0,0 +1,152 @@
+'''
+
+I used a package called seaborn that you need to install to run this code.
+You can install it this way:
+    pip install --user seaborn
+
+'''
+import pandas as pd
+import random as rd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+import inc.activationFunctions as af
+from inc.perceptron import Perceptron
+from inc.adaline import Adaline
+
+def trainPerceptron(df, minLearningRate = 0.005, maxIter = 100):
+    neuron = Perceptron()
+    acc = 0
+    iter = 0
+    learningRate = 1
+    lastAccuracies = list()
+    for i in range(10):
+        lastAccuracies.append(0)
+        lastAccuracies.append(1)
+
+    # Track error during training
+    errorEvolution = list()
+
+    # Generate random weights and biases:
+    radius = 5
+    neuron.weights = list()
+    for i in range(df.shape[1] - 1):
+        neuron.weights.append(rd.uniform(-radius, radius))
+
+                    #rd.uniform(-radius, radius), rd.uniform(-radius,radius)]
+    neuron.bias = rd.uniform(-radius, radius)
+
+    while (learningRate > minLearningRate) and (iter < maxIter):
+        # Loop over dataset once, update weights and return accuracy
+        acc = iteratePerceptronTraining(df, neuron)
+
+        # Update learningRate
+        del lastAccuracies[0]
+        lastAccuracies.append(acc)
+        learningRate = max(lastAccuracies) - min(lastAccuracies)
+
+        # Update error
+        errorEvolution.append(1-acc)
+        # Update iteration counter
+        iter += 1
+
+    return neuron, errorEvolution
+
+# Loop over dataset once, update weights and return accuracy
+def iteratePerceptronTraining(df, neuron, weightUpdateStep = 1):
+    hits = 0
+    for index, row in df.iterrows():
+        # Classify instance
+        inputs = row[0:-1]
+        neuronOutput = neuron.run(inputs)
+        if neuronOutput > 0.5:
+            classification = 2
+        else:
+            classification = 1
+
+        # Update weights
+        trueClass = row[-1]
+        for i in range(len(neuron.weights)):
+            neuron.weights[i] += weightUpdateStep * inputs[i] * (trueClass - classification)
+
+        # Update hits
+        if trueClass == classification:
+            hits += 1
+
+    return hits/len(df)
+
+def trainAdaline(df, minLearningRate = 0.005, maxIter = 100):
+    neuron = Adaline()
+    acc = 0
+    iter = 0
+    learningRate = 1
+    lastAccuracies = list()
+    for i in range(10):
+        lastAccuracies.append(0)
+        lastAccuracies.append(1)
+
+    # Track error during training
+    errorEvolution = list()
+
+    # Generate random weights and biases:
+    radius = 5
+    neuron.weights = list()
+    for i in range(df.shape[1] - 1):
+        neuron.weights.append(rd.uniform(-radius, radius))
+
+    neuron.bias = rd.uniform(-radius, radius)
+
+    while (learningRate > minLearningRate) and (iter < maxIter):
+        # Loop over dataset once, update weights and return accuracy
+        acc = iterateAdalineTraining(df, neuron)
+
+        # Update learningRate
+        del lastAccuracies[0]
+        lastAccuracies.append(acc)
+        learningRate = max(lastAccuracies) - min(lastAccuracies)
+
+        # Update error
+        errorEvolution.append(1-acc)
+        # Update iteration counter
+        iter += 1
+
+    return neuron, errorEvolution
+
+# Loop over dataset once, update weights and return accuracy
+def iterateAdalineTraining(df, neuron, weightUpdateStep = 1):
+    hits = 0
+    for index, row in df.iterrows():
+        # Classify instance
+        inputs = row[0:-1]
+        neuronOutput = neuron.run(inputs)
+        if neuronOutput > 0.5:
+            classification = 2
+        else:
+            classification = 1
+
+        # Update weights
+        trueClass = row[-1]
+        for i in range(len(neuron.weights)):
+            neuron.weights[i] += weightUpdateStep * inputs[i] * (trueClass - (neuronOutput + 1))
+
+        # Update hits
+        if trueClass == classification:
+            hits += 1
+
+    return hits/len(df)
+
+def main():
+    # Dataset 1
+    # Show dataset
+    df = pd.read_csv('../data/Aula3-dataset_1.csv')
+    sns.pairplot(df, hue=df.columns[-1])
+    plt.show()
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
